6745cb8252c8ac9a2e3d36a5ee6795321c635478
Tharindu37 <tharindut520@gmail.com>
2024-03-23 14:11:01 +0530

    image crop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ImageCroppedEvent, ImageCropperModule } from 'ngx-image-cropper';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageCropperModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  imgChangeEvt: any = '';
+  cropImgPreview: any;
+  image: File | undefined;
+
   title = 'Crop_Scale_Resize_Image_Angular';
+
+  onFileChange(evnet: any) {
+    this.imgChangeEvt = event;
+  }
+
+  cropImg(e: ImageCroppedEvent) {
+    const blob = e.blob;
+    const reader = new FileReader();
+    reader.onload = (event: any) => {
+      // Assign the base64-encoded image data to cropImgPreview
+      this.cropImgPreview = event.target.result;
+    };
+    reader.readAsDataURL(blob!);
+    this.image = new File([blob!], 'crop_profile', { type: 'image/png' });
+  }
+
+  imgLoad() {}
+
+  initCropper() {}
+
+  imgFailed() {
+    alert('Image failed to show');
+  }
 }
